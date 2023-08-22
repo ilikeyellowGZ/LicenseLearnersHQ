588bd7d4838c6a03507e5f1ee4f119648be9e893
ilikeyellowGZ <thisforthetimewehad@gmail.com>
2023-08-22 20:54:44 +0200

    you doneo

--- testFolder/index.js
+++ testFolder/index.js
@@ -1,34 +1,122 @@
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const alreadyDone = document.querySelectorAll(".btn");
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
+function startGame() {
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+    if (selectedButton === correct) {
+    } else {
+      selectedButton.style.color = "white";
+      selectedButton.style.backgroundColor = "red";
+    }
+  });
+
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+    // the email function should be right here becaUSE this says when the test is finished remove hide somewhere and restarts
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+//writing my own function for the one the user selected(wish me luck) i had to move my function up by a couple lines
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+//settting the right and wrong class in the css, i think making the backgrounds diffirent colours
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
 const questions = [
   {
-    question: "what colour is a banana?",
+    question: "What is 2 + 2?",
     answers: [
-      { text: "red", correct: false },
-      { text: "brown", correct: false },
-      { text: "Yellow", correct: true },
-      { text: "blue", correct: false },
+      { text: "4", correct: true },
+      { text: "22", correct: false },
     ],
   },
   {
-    question: "who has the biggest Glizzy in the world",
+    question: "how big is yout glizzy",
     answers: [
-      { text: "Ronaldo", correct: false },
-      { text: "Jamal", correct: true },
-      { text: "Messi", correct: false },
-      { text: "Sam", correct: false },
+      { text: "small (sigh)", correct: true },
+      { text: "microphobia type", correct: true },
+      { text: "average", correct: false },
+      { text: "extra tonka", correct: false },
+    ],
+  },
+  {
+    question: "do you like men?",
+    answers: [
+      { text: "Kinda", correct: false },
+      { text: "YES!!!", correct: true },
+      { text: "Um no", correct: false },
+      { text: "IDK", correct: false },
+    ],
+  },
+  {
+    question: "whats your favourite word?",
+    answers: [
+      { text: "nigga", correct: true },
+      { text: "glizzy", correct: false },
     ],
   },
 ];
-
-const questionsElement = document.getElementsByClassName("questionsHere");
-const answerElement = document.getElementsByClassName("answerWrapper");
-const nextBtn = document.getElementsById("next-btn");
-
-let currentQuestionIndex = 0;
-let currentScore = 0;
-
-function startQuiz() {
-  currentQuestionIndex = 0;
-  currentScore = 0;
-  nextBtn.innerHTML = "Next";
-  showQuestion();
-}
