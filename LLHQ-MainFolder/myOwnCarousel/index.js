097c1d7660ac72203e7e8a03b208ea5886e14dd2
ilikeyellowGZ <thisforthetimewehad@gmail.com>
2023-08-25 16:25:07 +0200

    imdone4now

---
+++ LLHQ-MainFolder/myOwnCarousel/index.js
@@ -0,0 +1,15 @@
+/* const swiperSLide = document.querySelectorAll(".swiper-slide ");
+const swiperLeft = document.querySelectorAll(".swiper .swiper-button-prev");
+const swiperRight = document.querySelectorAll(".swiper .swiper-button-prev ");
+
+function startAnimation() {}
+
+swiperLeft.addEventListner("click" , ()=> {
+    swiperSLide.classList.add("swiper-slide-next")
+})
+swiperRight.addEventListner("click" , ()=> {
+    swiperSLide.classList.add("swiper-slide-next")
+})
+
+
+ */
